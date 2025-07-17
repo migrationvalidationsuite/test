ea666d80bc8fb796d83ed180812b3cf0dcb37e7a
MVS <56733647+migrationvalidationsuite@users.noreply.github.com>
2025-07-17 19:37:12 +1000

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from streamlit_option_menu import option_menu
 from foundation_module.foundation_app import render as render_foundation
 from employee_app import render_employee_tool
 from employeedata.app.data_migration_tool import render_employee_v2
+from payroll.app import render_payroll_tool
+
 
 # ✅ Correct usage
 render_payroll_tool()
